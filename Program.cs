4c702662ecf77cad1cd76c4bf6964585dfd85975
AlexLopez75 <alex.lopez.7e9@itb.cat>
2025-11-26 18:39:39 +0100

    feat: add program funcionality for exercise 1

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        const string MsgInputOne = "Input a natural number: ";
+        const string MsgCorrect = "The number is between the range of valid numbers.";
+        const string MsgIncorrect = "The number isn't between the range of valid numbers.";
+
+        int number;
+
+        do
+        {
+            Console.Write(MsgInputOne);
+            try
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+                if (!NumberRange(number))
+                {
+                    Console.WriteLine(MsgIncorrect);
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(MsgIncorrect);
+                number = 0;
+            }
+        } while (!NumberRange(number));
+
+        Console.WriteLine(MsgCorrect);
+    }
+
+    public static bool NumberRange(int value)
+    {
+        const int MinRange = 10;
+        const int MaxRange = 50;
+
+        return value >= MinRange && value <= MaxRange;
+    }
+}
